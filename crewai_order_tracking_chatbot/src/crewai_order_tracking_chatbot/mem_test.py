adab49c321303c219753fa1d3147522b3c2fdf01
Arhan-SE <zareenabanu6699@gmail.com>
2025-03-25 07:23:03 +0000

    .

--- crewai_order_tracking_chatbot/src/crewai_order_tracking_chatbot/mem_test.py
+++ crewai_order_tracking_chatbot/src/crewai_order_tracking_chatbot/mem_test.py
@@ -14,7 +14,7 @@ config = {
 
 memory = Memory.from_config(config)
 
-relevant_info=memory.search(query="Tell me the details of order number 936848", limit=10, user_id="default_user")
+relevant_info=memory.search(query="Tell me the details of order number 936848", limit=6, user_id="default_user")
 context_lst=[entry["memory"] for entry in relevant_info['results']]
 
 for context in context_lst:
